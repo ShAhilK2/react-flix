5aee4a8c06be04389403ee0118c9bcaaee3a2495
ShAhilK2 <shahilkataria852002@gmail.com>
2025-10-06 21:37:49 +0530

    feat : Header and Hero Component

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Movies from "./components/Movies";
+
+const App = () => {
+  return (
+    <main>
+      <div className="min-h-screen bg-background text-foreground transition-colors duration-300">
+        <Header />
+        <Hero />
+        <Movies />
+      </div>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import heroBg from "../assets/hero.jpg";
+const Header = () => {
+  return (
+    <header className="absolute top-0 left-0 right-0 z-[100]  py-5 ">
+      <div className="container max-w-6xl mx-auto px-6 md:px-6 flex justify-between items-center">
+        <div className="flex-shrink-0">
+          <h1 className="md:text-md lg:text-3xl  font-bold   !text-red-600 ">
+            REACTFLIX
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
